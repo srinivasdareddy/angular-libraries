1f523a59741ad533252b21076467337cbf9e51d7
srinivasdhareddy <srinivasdareddy@gmail.com>
2017-06-17 10:01:12 -0700

    todo add extra large breakpoint

--- src/app/srlib/constants.ts
+++ src/app/srlib/constants.ts
@@ -4,5 +4,6 @@ export class Breakpoints {
   static BREAKPOINT_TABLET_LANDSCAPE = {name: 'TABLET_LANDSCAPE', value: 1024};
   static BREAKPOINT_MEDIUM_DESKTOP = {name: 'DESKTOP', value: 1280};
   static BREAKPOINT_LARGE_DESKTOP = {name: 'LARGE_DESKTOP', value: 1920};
+  // todo add extra large breakpoint
   static BREAKPOINT_EXTRA_LARGE_DESKTOP = {name: 'EXTRA_LARGE_DESKTOP', value: 1921};
 }
